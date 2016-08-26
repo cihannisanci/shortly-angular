bd1a706712e051a16b9aeca06d50dab465e1a3ea
cihannisanci <cihannisan@gmail.com>
2016-08-26 16:17:48 -0700

    Add Links Factory functionality for GET

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,29 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  return {
+    getAll: function() {
+      return $http({
+        method: 'GET',
+        url: '/api/links'
+      })
+      .then(function(resp) {
+        return resp.data;
+      });
+    },
+    addOne: function(link) {
+      return $http({
+        method: 'POST',
+        url: '/api/links',
+        data: {
+          url: link
+        }
+      })
+      .then(function(resp) {
+        return resp.data;
+      });
+    }
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
