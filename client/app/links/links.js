516f5c59fd31bf4fb0226a18b7b037daf51e02ee
cihannisanci <cihannisan@gmail.com>
2016-08-26 19:02:28 -0700

    Add Link and Shorten views and fix routing

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,10 +1,12 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $location) {
   $scope.data = {};
   Links.getAll().then(function(data) {
     $scope.data.links = data;
+  }).then(function() {
+    $location.path('/links');
+    
   });
-
 
 });
