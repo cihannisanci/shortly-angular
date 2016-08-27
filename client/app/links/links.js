0fa776f46693b1fb9e1c632be68fbc1a7fbf1f79
cihannisanci <cihannisan@gmail.com>
2016-08-26 20:04:36 -0700

    Authenticate shorten and links views and set default view (links)

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,11 +1,17 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links, $location) {
+.controller('LinksController', function ($scope, Links, $location, Auth) {
   $scope.data = {};
   Links.getAll().then(function(data) {
     $scope.data.links = data;
   }).then(function() {
-    $location.path('/links');
+    return Auth.isAuth();
+  }).then(function(isAuth) {
+    if (isAuth) {
+      $location.path('/links');
+    } else {
+      $location.path('/signin');
+    }
     
   });
 
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,9 +1,14 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link = {};
   $scope.addLink = function(link) {
     var linkObj = {url: link};
     Links.addOne(linkObj);
   };
+  if (Auth.isAuth()) {
+    $location.path('/shorten');
+  } else {
+    $location.path('/signin');
+  }
 });
