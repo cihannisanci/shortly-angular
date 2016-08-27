4890149429015cc6c20e995fa2823331f876055b
Brian Kilrain <bkilrain@gmail.com>
2016-08-27 16:31:41 -0700

    ui-router implementation init

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,8 @@ angular.module('shortly', [
   'shortly.links',
   'shortly.shorten',
   'shortly.auth',
-  'ngRoute'
+  'ngRoute',
+  'ui.router'
 ])
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
